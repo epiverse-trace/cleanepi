0e4aaab3e326f9c9a41a87ab24ca6ee684ad86f9
Karim-Mane <karimanee@outlook.com>
2023-09-28 13:31:28 +0000

    account for Hugo's suggestions

--- tests/testthat/setup-options.R
+++ tests/testthat/setup-options.R
@@ -1,10 +1,6 @@
 # We want to flag partial matching as part of our testing & continuous
 # integration process because it makes code more brittle.
-<<<<<<< HEAD
 options( # nolint: undesirable_function_linter
-=======
-options(
->>>>>>> 65a523d (Fix partial matching warning (#22))
   warnPartialMatchAttr = TRUE,
   warnPartialMatchDollar = TRUE,
   warnPartialMatchArgs = TRUE
