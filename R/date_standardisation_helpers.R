36bf603538125b744b9b63b069d144aea945dc66
Karim MANE <84502011+Karim-Mane@users.noreply.github.com>
2023-09-28 12:58:31 +0000

    Update R/date_standardisation_helpers.R
    Co-authored-by: Hugo Gruson <Bisaloo@users.noreply.github.com>

--- R/date_standardisation_helpers.R
+++ R/date_standardisation_helpers.R
@@ -355,10 +355,10 @@ get_format <- function(data, date_column_name, sep) {
 #' @noRd
 #'
 make_format <- function(f1, f2, tmp_sep) {
-  if (all(is.null(f1) & is.null(f2))) {
+  if (is.null(f1) && is.null(f2)) {
     stop("Unrecognised date format.\nPlease specify the date format using
          the 'format' argument.")
-  } else if (all(!is.null(f1) & !is.null(f2))) {
+  } else if (!is.null(f1) && !is.null(f2)) {
     format <- paste0(f1, tmp_sep, f2)
   } else if (!is.null(f1) && is.null(f2)) {
     format <- f1
