26f23c3018f8c783f351c85b2c84d20e0c9fba33
Karim-Mane <karimanee@outlook.com>
2024-01-25 10:38:21 +0000

    run devtools::document()

--- R/standardize_date.R
+++ R/standardize_date.R
@@ -1,48 +1,41 @@
 #' Standardize date variables
 #'
 #' @param data the input data frame
-#' @param date_column_name the name of the date column of interest.
+#' @param target_columns a vector of targeted date column names.
 #'    default: 'Date', or 'DATE', or 'date'
-#' @param format the format of the date values in the date column
-#' @param check_timeframe a logical to check whether dates fall within timeframe
-#' @param timeframe the expected first and last date. See the `clean_data()`
-#'    helper for more details
-#' @param report the object that will contains details about the result from the
-#'    date columns standardization
+#' @param format the format of the date values in the date columns
+#' @param timeframe a vector of 2 values of type date. If provided, date values
+#'    that do not fall within this timeframe will be set to `NA`.
 #' @param error_tolerance a number between 0 and 1 indicating the proportion of
 #'     entries which cannot be identified as dates to be tolerated. See the
-#'     `clean_data()` helper for more details
+#'     `clean_data()` helper for more details.
 #'
-#' @returns a `list` of 2 elements: the input dataset where the date columns
-#'    have been standardized and a report object of type `list` that contains
-#'    the details about the columns that have been modified.
-#'
+#' @returns the input dataset where the date columns have been standardized
 #' @export
+#'
 #' @examples
-#' dat <- standardize_date(
-#' data = readRDS(system.file("extdata", "test_df.RDS", package = "cleanepi")),
-#' date_column_name = "date_first_pcr_positive_test",
-#' format = NULL,
-#' timeframe = NULL,
-#' check_timeframe = FALSE,
-#' report = list(),
-#' error_tolerance = 0.5
+#' dat <- standardize_dates(
+#'   data            = readRDS(system.file("extdata", "test_df.RDS",
+#'                                         package = "cleanepi")),
+#'   target_columns  = "date_first_pcr_positive_test",
+#'   format          = NULL,
+#'   timeframe       = NULL,
+#'   error_tolerance = 0.5
 #' )
-standardize_date <- function(data, date_column_name = NULL, format = NULL,
-                             timeframe = NULL, check_timeframe = FALSE,
-                             report = list(), error_tolerance = 0.5) {
+standardize_dates <- function(data,
+                             date_column_name = NULL,
+                             format           = NULL,
+                             timeframe        = NULL,
+                             error_tolerance  = 0.5) {
   checkmate::assert_data_frame(data, null.ok = FALSE, min.cols = 1L)
   checkmate::assert_character(date_column_name, null.ok = TRUE,
                               any.missing = FALSE)
   checkmate::assert_character(format, null.ok = TRUE, any.missing = FALSE)
-  checkmate::assert_logical(check_timeframe, any.missing = FALSE,
-                            null.ok = FALSE, len = 1L)
   checkmate::assert_date(timeframe, any.missing = FALSE,
                          null.ok = TRUE, len = 2L, unique = TRUE)
   checkmate::assert_numeric(error_tolerance, lower = 0L, upper = 1L,
                             max.len = 2L,
                             any.missing = FALSE, null.ok = TRUE)
-  checkmate::assert_list(report, any.missing = FALSE, null.ok = FALSE)
 
   if (!is.null(date_column_name)) {
     # check input data format
