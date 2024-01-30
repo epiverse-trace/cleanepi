3ed1142e78513ff72275c224a75f0ffd9e7d9cb9
= <degoot@aimsa.ac.za>
2024-01-30 10:38:40 +0000

    changed "age_years" to "age_in_years", simillarly for months, weeks, and days.

--- R/calcualte_age.R
+++ R/calcualte_age.R
@@ -1,18 +1,18 @@
 #' function to calculate age from date of birth
-#' @param data a data frame with the date column to standardise
+#' @param data a data frame with at one date column.
 #' @param date_column_name the name of the date column of interest.
 #' default: 'Date', or, 'DATE', or 'date'
 #' @param end_date the end date. default: today's date
 #' @param age_in a character string to specify whether to calculate
 #'  the age in 'years', or 'months', or 'days', or 'weeks'.
-#' default is: 'years'
+#' default is: 'years'.
 #' @returns a data frame with 1 or 2 extra columns compared to the input data
-#' frame
+#' frame.
 #' \enumerate{
-#'   \item "age_years", or "age_months", or "age_weeks", or "age_days",
-#' depending on the value of the 'age_in' parameter
+#'   \item "age_in_years", or "age_in_months", or "age_in_weeks", or "age_in_days",
+#' depending on the value of the 'age_in' parameter.
 #'   \item "remainder_days": a column with the number of remaining days
-#'  after the age is converted in weeks or months
+#'  after the age is converted in weeks or months.
 #'   }
 #' @export
 #' @examples
@@ -50,8 +50,8 @@ calculate_age <- function(data, date_column_name = NULL, end_date = Sys.Date(),
   # calculate age
   if (!(age_in %in% c("years", "months", "weeks", "days"))) {
     stop("Incorrect value for 'age_in' parameter.\n",
-         "Please specify whether the age should be returned in 'year', or",
-         "'month', or in 'weeks', or in 'days'.")
+         "Please specify whether the age should be returned in 'years', or",
+         "'months', or in 'weeks', or in 'days'.")
   }
   end_date <- as.Date(end_date)
 
