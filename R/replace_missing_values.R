d4fedf271b0731c24883f124f0519723f96f2739
Karim-Mane <karimanee@outlook.com>
2024-02-01 10:05:32 +0000

    update replace_missing_values() to allow users have control over the common NA strings

--- R/replace_missing_values.R
+++ R/replace_missing_values.R
@@ -2,10 +2,12 @@
 #' Replace missing values with `NA`
 #'
 #' @param data the input data
-#' @param from a vector of column names. If provided, the missing values
-#'    substitution will only be performed in those columns
-#' @param na_comes_as the string that represents the missing values in the
-#'    column of interest
+#' @param target_columns a vector of column names. If provided, the missing
+#'    values substitution will only be performed in those columns.
+#' @param na_strings a vector of strings that represent the missing values in
+#'    the columns of interest. Default values are `cleanepi::common_na_strings`.
+#'    When the missing values string in the columns of interest is not part of
+#'    this predefined vector, it can be used as the value for this argument.
 #'
 #' @return the input data where missing values are replaced by `NA`
 #' @export
@@ -14,47 +16,38 @@
 #' cleaned_data <- replace_missing_values(
 #'   data        = readRDS(system.file("extdata", "test_df.RDS",
 #'                                     package = "cleanepi")),
-#'   from        = "sex",
-#'   na_comes_as = "-99"
+#'   target_columns = "sex",
+#'   na_strings     = "-99"
 #' )
 #'
-replace_missing_values <- function(data, from = NULL, na_comes_as = NULL) {
-  # define the common NA strings
-  common_na_string <- c(naniar::common_na_strings, "not available",
-                        "Not Available", "NOt available", "not avail",
-                        "Not Avail", "nan", "NAN", "not a number",
-                        "Not A Number", "-99")
-
+replace_missing_values <- function(data,
+                                   target_columns = NULL,
+                                   na_strings = cleanepi::common_na_strings) {
   # get the target columns
   index        <- seq_len(ncol(data))
-  if (!is.null(from)) {
-    index      <- which(names(data) == from)
+  if (!is.null(target_columns)) {
+    index      <- which(names(data) == target_columns)
   }
   cols         <- names(data)[index]
 
   # replace missing values with NA
-  res <- 0L
+  res     <- 0L
   indexes <- NULL
   for (col in cols) {
     index              <- match(col, names(data))
     names(data)[index] <- "x"
-    if (!is.null(na_comes_as)) {
-      data   <- naniar::replace_with_na(data, replace = list(x = na_comes_as))
+    idx    <- which(na_strings %in% data[["x"]])
+    if (length(idx) > 0L) {
+      data <- naniar::replace_with_na(data, replace = list(x = na_strings[idx]))
+      indexes <- c(indexes, col)
     } else {
-      idx    <- which(common_na_string %in% data[["x"]])
-      if (length(idx) > 0L) {
-        data <- naniar::replace_with_na(data,
-                                        replace = list(x = common_na_string[idx])) # nolint: line_length_linter
-        indexes <- c(indexes, col)
-      } else {
-        res  <- res + 1L
-      }
-      names(data)[index] <- col
+      res  <- res + 1L
     }
+    names(data)[index] <- col
   }
-  stopifnot("Could not detect missing value character! Please provide the
-                character that represents the missing values." =
-              res < ncol(data))
+  stopifnot("Could not detect missing value character! Please use the
+                appropriate strings that represents the missing values from
+                your data." = res < ncol(data))
 
   # make report
   xx   <- ifelse(length(indexes) == ncol(data),
