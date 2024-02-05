7f43bb85b7b5843a258fd171e9fc116f4b75fd28
= <degoot@aimsa.ac.za>
2024-02-05 09:47:36 +0000

    No default for date_column_name

--- R/calcualte_age.R
+++ R/calcualte_age.R
@@ -1,25 +1,21 @@
 #' Calculate age from a specified date column
 #'
-#' @param data the input data frame with the date column of interest
-#' @param target_column the name of the date column of interest. default are
-#'    'Date', or, 'DATE', or 'date'
-#' @param end_date the end date. default: today's date
-#' @param age_in a string that specifies whether to return the age in 'years',
-#'    or 'months', or 'days', or 'weeks'. Default is 'years'.
-#' @param ... Other extra arguments needed to perform this operation. They
-#'    include:
-#'    \enumerate{
-#'      \item "na_strings": a string that represents the missing values in the
-#'            date column of interest. This is only needed when the date column
-#'            contains missing values.
-#'   }
+#' @param data A data frame with at least one date column.
+#' @param target_column A string specifying the name of the date column of
+#'    interest.
+#' @param end_date An end date, the default is today's date.
+#' @param age_in A string that specifies whether to return the age in 'years',
+#'    'months', 'weeks' or 'days'. The default is in 'years'.
+#' @param na_strings A string that represents the missing values in the
+#'    date column of interest. This is only needed when the date column
+#'    contains missing values.
 #'
-#' @returns the input data frame with the following 1 or 2 extra columns:
+#' @return The input data frame with 2 additional columns:
 #' \enumerate{
-#'   \item "age_in_years", or "age_in_months", or "age_in_weeks", or
-#'         "age_in_days", depending on the value of the 'age_in' parameter.
-#'   \item "remainder_days": a column with the number of remaining days after
-#'         the age is converted in weeks or months.
+#'   \item "age_in_years",  "age_in_months",  "age_in_weeks", or
+#'         "age_in_days" depending on the value of the 'age_in' parameter.
+#'   \item "remainder_days": A column with the number of remaining days after
+#'         the age is calculated in years, weeks or months.
 #'   }
 #' @export
 #'
@@ -36,67 +32,62 @@ calculate_age <- function(data,
                           target_column = NULL,
                           end_date      = Sys.Date(),
                           age_in        = "years",
-                          ...) {
+                          na_strings    = cleanepi::common_na_strings) {
   checkmate::assert_data_frame(data, null.ok = FALSE)
-  checkmate::assert_character(target_column, null.ok = TRUE,
-                              any.missing = FALSE, len = 1L)
-  checkmate::assert_character(age_in, null.ok = FALSE, any.missing = FALSE,
-                              len = 1L)
-  checkmate::assert_date(end_date, any.missing = FALSE, len = 1L,
-                         null.ok = TRUE)
-
+  checkmate::assert_vector(na_strings, null.ok = FALSE,
+                           any.missing = FALSE, min.len = 1L)
+  checkmate::assert_choice(age_in,
+                           choices = c("years", "months", "weeks", "days"),
+                           null.ok = FALSE)
+  checkmate::assert_choice(target_column, choices = colnames(data),
+                           null.ok = TRUE)
+  end_date <- checkmate::assert_date(as.Date(end_date),
+                                     any.missing = FALSE, len = 1L,
+                                     null.ok = TRUE)
   tmp_age <- remainder_days <- NULL
-  extra_args <- list(...)
-
-  # check if date column exists in the data
-  target_column <- date_check_column_existence(data, target_column)
 
   # replace missing data characters with NA
-  if ("na_strings" %in% names(extra_args)) {
-    na_strings  <- extra_args[["na_strings"]]
-  }
-  data          <- replace_missing_values(data, target_column,
-                                          na_strings = na_strings)
+  data <- replace_missing_values(data, target_column,
+                                 na_strings = na_strings)
 
   # standardize the input data if required
   if (!lubridate::is.Date(data[[target_column]])) {
-    data        <- standardize_dates(data, target_column, format = NULL,
-                                     timeframe = NULL, error_tolerance = 0.5)
+    data <- standardize_dates(data, target_column,
+                              format          = NULL,
+                              timeframe       = NULL,
+                              error_tolerance = 0.0) # because target_column is explicit
   }
-
-  # calculate age
-  if (!(age_in %in% c("years", "months", "weeks", "days"))) {
-    stop("Incorrect value for 'age_in' parameter.\n",
-         "Please specify whether the age should be returned in 'years', or",
-         "'months', or in 'weeks', or in 'days'.")
-  }
-  end_date <- as.Date(end_date)
 
   # calculate the age
-  res <- switch(
-    age_in,
-    years = data %>%
-      dplyr::mutate(age_years = round((data[[target_column]] %--% end_date)
-                                      %/% lubridate::years(1L))),
-    months = data %>%
-      dplyr::mutate(tmp_age = lubridate::as.period(end_date -
-                                                     data[[target_column]])) %>% # nolint: line_length_linter.
-      dplyr::mutate(age_months = tmp_age %/% months(1L), # nolint
-                    remainder_days = (tmp_age %% months(1L)) %/% # nolint
-                      lubridate::days(1L)) %>%
-      dplyr::select(-tmp_age),
-    days = data %>%
-      dplyr::mutate(tmp_age = lubridate::as.period(end_date -
-                                                     data[[target_column]])) %>% # nolint: line_length_linter.
-      dplyr::mutate(age_days = tmp_age %/% lubridate::days(1L)) %>%
-      dplyr::select(-tmp_age),
-    weeks = data %>%
-      dplyr::mutate(tmp_age = lubridate::as.period(end_date -
-                                                     data[[target_column]])) %>% # nolint: line_length_linter.
-      dplyr::mutate(age_weeks = tmp_age %/% lubridate::weeks(1L),
+  res <- switch(age_in,
+                years = data %>%
+                  dplyr::mutate(age_years = round((data[[target_column]] %--% end_date)
+                                                  %/% lubridate::years(1L))),
+                months = data %>%
+                  dplyr::mutate(tmp_age = lubridate::as.period(end_date -
+                                                                 data[[target_column]])) %>%
+                  dplyr::mutate(
+                    age_months = tmp_age %/% months(1L, abbreviate = FALSE),
+                    remainder_days = (tmp_age %% months(1L, abbreviate = FALSE)) %/%
+                      lubridate::days(1L)
+                  ) %>%
+                  dplyr::select(-tmp_age),
+                days = data %>%
+                  dplyr::mutate(tmp_age = lubridate::as.period(end_date -
+                                                                 data[[target_column]])) %>%
+                  dplyr::mutate(
+                    age_days = tmp_age %/% lubridate::days(1L)
+                  ) %>%
+                  dplyr::select(-tmp_age),
+                weeks = data %>%
+                  dplyr::mutate(tmp_age = lubridate::as.period(end_date -
+                                                                 data[[target_column]])) %>%
+                  dplyr::mutate(
+                    age_weeks = tmp_age %/% lubridate::weeks(1L),
                     remainder_days = (tmp_age %% lubridate::weeks(1L))
-                    %/% lubridate::days(1L)) %>%
-      dplyr::select(-tmp_age)
+                    %/% lubridate::days(1L)
+                  ) %>%
+                  dplyr::select(-tmp_age)
   )
   if (age_in %in% c("months", "weeks") && all(res[["remainder_days"]] == 0L)) {
     res <- res %>% dplyr::select(-remainder_days)
