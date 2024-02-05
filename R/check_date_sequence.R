6020e4d2d2d310aa18d266f123e59c6a663e5bf2
Karim-Mane <karimanee@outlook.com>
2024-02-05 10:19:26 +0000

    update the check date sequence module

--- R/check_date_sequence.R
+++ R/check_date_sequence.R
@@ -2,102 +2,89 @@
 #'
 #' @param x the string of interest
 #' @keywords internal
-#' @noRd
 is_order <- function(x) {
   x <- as_date(x)
-  !is.unsorted(x)
+  return(!is.unsorted(x))
 }
 
 
 
-#' Check order for sequence of date-events
+#' Check whether the order of the sequence of date-events is valid
 #'
 #' @description This function checks whether a date sequence in
 #' a vector of  specified columns is correct or not.
 #'
 #' @param data the input data frame
-#' @param event_cols a vector or a comma-separated list of the event columns
+#' @param target_columns a vector or a comma-separated list of the event columns
 #'    names. Users should specify at least 2 column names in the expected order.
-#'    For example: event_cols = c("date_symptoms_onset", "date_hospitalization", "date_death").
+#'    For example: target_columns = c("date_symptoms_onset", "date_hospitalization", "date_death"). # nolint: line_length_linter
 #' @param remove_bad_seq a Boolean to specify if rows with incorrect order
 #'    should be filtered out or not. The default is FALSE
-#' @param report  object  will contain details about the results from the
-#'    date columns standardization
 #'
 #' @returns rows of the input data frame with incorrect date sequence
 #'    if `remove_bad_seq = FALSE`, the input data frame without those
-#'    rows if not
+#'    rows if not.
 #' @export
 #'
 #' @examples
 #' good_date_sequence <- check_date_sequence(
-#' data = readRDS(system.file("extdata", "test_df.RDS", package = "cleanepi")),
-#' event_cols = c("date_first_pcr_positive_test", "date.of.admission"),
-#' remove_bad_seq = FALSE,
-#' report = list()
+#'   data           = readRDS(system.file("extdata", "test_df.RDS",
+#'                                        package = "cleanepi")),
+#'   target_columns = c("date_first_pcr_positive_test", "date.of.admission"),
+#'   remove_bad_seq = FALSE
 #' )
-check_date_sequence <- function(data, event_cols, remove_bad_seq = FALSE,
-                                report = list()) {
-  checkmate::assert_vector(event_cols, any.missing = FALSE, min.len = 1L, max.len = dim(data)[2],
-                           null.ok = FALSE, unique = TRUE)
+check_date_sequence <- function(data, target_columns,
+                                remove_bad_seq = FALSE) {
+
+  checkmate::assert_vector(target_columns, any.missing = FALSE, min.len = 1L,
+                           max.len = dim(data)[2], null.ok = FALSE,
+                           unique = TRUE)
   checkmate::assert_data_frame(data, null.ok = FALSE)
   checkmate::assert_logical(remove_bad_seq, any.missing = FALSE, len = 1L,
                             null.ok = FALSE)
 
   # check if input is character string
-  if (all(grepl(",", event_cols, fixed = TRUE))) {
-    event_cols <- as.character(unlist(strsplit(event_cols, ",", fixed = TRUE)))
-    event_cols <- gsub(" ", "", event_cols, fixed = TRUE)
+  if (all(grepl(",", target_columns, fixed = TRUE))) {
+    target_columns <- as.character(unlist(strsplit(target_columns, ",", fixed = TRUE)))
+    target_columns <- trimws(target_columns)
   }
 
   # check if all columns are part of the data frame
-  if (!all(event_cols %in% names(data))) {
-    idx <- which(!(event_cols %in% names(data)))
-    event_cols <- event_cols[-idx]
-    warning("\nRemoving unrecognised column name: ", event_cols[idx],
+  if (!all(target_columns %in% names(data))) {
+    idx            <- which(!(target_columns %in% names(data)))
+    target_columns <- target_columns[-idx]
+    warning("\nRemoving unrecognised column name: ", target_columns[idx],
             call. = FALSE)
-    if (length(event_cols) < 2L) {
+    if (length(target_columns) < 2L) {
       stop("\nAt least 2 event dates are required!")
     }
   }
 
   # check and convert to Date if required
-  for (cols in event_cols) {
+  for (cols in target_columns) {
     if (!lubridate::is.Date(data[[cols]])) {
-      data <- standardize_date(data, cols, timeframe = NULL,
-                               check_timeframe = FALSE,
-                               report = list(), error_tolerance = 0.5)[[1L]]
+      data <- standardize_dates(data, cols, timeframe = NULL,
+                                error_tolerance = 0.5)
     }
   }
 
   # checking the date sequence
-  tmp_data <- data %>% dplyr::select(dplyr::all_of(event_cols))
+  tmp_data   <- data %>% dplyr::select(dplyr::all_of(target_columns))
   order_date <- apply(tmp_data, 1L, is_order)
-  bad_order <- which(!order_date)
+  bad_order  <- which(!order_date)
   if (!all(order_date)) {
     tmp_data <- data[bad_order, ]
+    # adding incorrect records to the report
+    data     <- add_to_report(x     = data,
+                              key   = "incorrect_date_sequence",
+                              value = tmp_data)
     if (remove_bad_seq) {
-      data <- data[-bad_order, ]
+      data  <- data[-bad_order, ]
       warning(length(bad_order),
               " incorrect date sequences were detected and removed",
               call. = FALSE)
     }
   }
 
-  # making the report
-  if (length(bad_order) > 0L) {
-    if (!("incorrect_date_sequence" %in% names(report))) {
-      report[["incorrect_date_sequence"]] <- list()
-    }
-    report[["incorrect_date_sequence"]][["date_sequence"]] <-
-      glue::glue_collapse(event_cols, sep = " < ")
-    report[["incorrect_date_sequence"]][["bad_sequence"]] <- tmp_data
-  } else {
-    report <- NULL
-  }
-
-  list(
-    data = data,
-    report = report
-  )
+  return(data)
 }
