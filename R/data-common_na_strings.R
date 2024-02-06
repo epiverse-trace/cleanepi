c8e05e605ddb4957622b1ff43256dc6f1af3b3db
= <degoot@aimsa.ac.za>
2024-02-06 08:35:52 +0000

    Documenting common NA strings

--- R/data-common_na_strings.R
+++ R/data-common_na_strings.R
@@ -1,13 +1,13 @@
-#' Common strings to represent missing values
+#' Common strings representing missing values
 #'
-#' This vector contains common values of NA (missing), which is aimed to
-#'   be used inside {cleanepi} functions [replace_missing_values()]. The current
-#'   list of strings used can be found by printing out `common_na_strings`. It
-#'   is a useful way to explore your data for possible missings, but I strongly
-#'   warn against using this to replace NA values without very carefully looking
-#'   at the incidence for each of the cases. Please note that
-#'   `common_na_strings` uses `\\` around the "?", "." and "*" characters to
-#'   protect against using their wildcard features in grep.
+#'This vector contains common values of NA (missing) and is intended for
+#' use within {cleanepi} functions [replace_missing_values()].
+#' The current list of strings used can be found by printing out
+#' `common_na_strings`. It serves as a helpful tool to explore your data
+#' for possible missing values. However, I strongly caution against using
+#'  this to replace `NA` values without meticulously examining the
+#'  incidence for each case. Please note that `common_na_strings` utilizes
+#'  `\\` around the "?", ".", and "*" characters to prevent their wildcard
 #'
 #' @source This vector is a combination of `naniar::common_na_strings`
 #'   (\url{https://github.com/njtierney/naniar/}) and other strings found in the
