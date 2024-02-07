560248af5a8a6f83892af0569929f07573572402
Karim-Mane <karimanee@outlook.com>
2024-02-07 18:30:04 +0000

    use relative package development path in print_report() to avoid errors

--- R/print_report.R
+++ R/print_report.R
@@ -6,9 +6,11 @@
 #' @importFrom utils browseURL
 print_report <- function(report) {
   checkmate::assert_list(report, min.len = 1L, null.ok = FALSE)
-  tmp_input  <- file.path(.libPaths(), "cleanepi", # nolint: undesirable_function_linter
-                          "rmarkdown", "templates", "test_printing-rmd",
-                          "skeleton", "skeleton.Rmd")
+  # tmp_input  <- file.path(.libPaths(), "cleanepi", # nolint: undesirable_function_linter
+  #                         "rmarkdown", "templates", "test_printing-rmd",
+  #                         "skeleton", "skeleton.Rmd")
+  tmp_input  <- file.path(".", "inst", "rmarkdown", "templates",
+                          "test_printing-rmd", "skeleton", "skeleton.Rmd")
   output_dir <- getwd()
   tmp_output <- file.path(output_dir,
                           "test_printing.html")
