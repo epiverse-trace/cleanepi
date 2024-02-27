029d6008ed1db8d25aac1c950015dd803744afb7
Karim-Mane <karimanee@outlook.com>
2024-02-27 17:41:04 +0000

    update the function documention in data-common_na_strings

--- R/data-common_na_strings.R
+++ R/data-common_na_strings.R
@@ -15,18 +15,5 @@
 #' @format A vector of 35 character strings.
 #' @name common_na_strings
 #' @docType data
-#' @examples
 #'
-#' dat_ms <- tibble::tribble(~x,  ~y,    ~z,
-#'                           1,   "A",   -100,
-#'                           3,   "N/A", -99,
-#'                           NA,  NA,    -98,
-#'                           -99, "E",   -101,
-#'                           -98, "F",   -1)
-#'
-#' miss_scan_count(dat_ms, -99)
-#' miss_scan_count(dat_ms, c("-99","-98","N/A"))
-#' common_na_strings
-#' miss_scan_count(dat_ms, common_na_strings)
-#' replace_with_na(dat_ms, replace = list(y = common_na_strings))
 "common_na_strings"
