121cc6e7ef11991939e2cedcecb4b6ec1921351f
Karim-Mane <karimanee@outlook.com>
2024-03-01 12:05:18 +0000

    Update test files to account for James suggestions

--- tests/testthat/test-replace_missing_values.R
+++ tests/testthat/test-replace_missing_values.R
@@ -1,16 +1,18 @@
 data <- readRDS(system.file("extdata", "test_df.RDS", package = "cleanepi"))
-test_that("replace_missing_values works", {
-  cleaned_data <- replace_missing_values(
-    data           = data,
-    target_columns = "sex",
-    na_strings     = "-99"
-  )
-  expect_s3_class(cleaned_data, "data.frame")
-  expect_false("-99" %in% cleaned_data[["sex"]])
-  expect_true(anyNA(cleaned_data[["sex"]]))
+test_that("replace_missing_values works when both target_columns and na_strings
+          are provided", {
+            cleaned_data <- replace_missing_values(
+              data           = data,
+              target_columns = "sex",
+              na_strings     = "-99"
+            )
+            expect_s3_class(cleaned_data, "data.frame")
+            expect_false("-99" %in% cleaned_data[["sex"]])
+            expect_true(anyNA(cleaned_data[["sex"]]))
+            expect_true(all(is.na(cleaned_data[["sex"]][data[["sex"]] == "-99"]))) # nolint: line_length_linter
 })
 
-test_that("replace_missing_values works", {
+test_that("replace_missing_values works when target_columns is set to NULL", {
   cleaned_data <- replace_missing_values(
     data           = data,
     target_columns = NULL,
@@ -19,8 +21,8 @@ test_that("replace_missing_values works", {
   expect_s3_class(cleaned_data, "data.frame")
   expect_false("-99" %in% cleaned_data[["sex"]])
   expect_false("-99" %in% cleaned_data[["dateOfBirth"]])
-  expect_true(anyNA(cleaned_data[["sex"]]))
-  expect_true(anyNA(cleaned_data[["dateOfBirth"]]))
+  expect_true(all(is.na(cleaned_data[["sex"]][data[["sex"]] == "-99"])))
+  expect_true(all(is.na(cleaned_data[["dateOfBirth"]][data[["dateOfBirth"]] == "-99"]))) # nolint: line_length_linter
 })
 
 test_that("replace_missing_values fails as expected", {
