a046e5a41f5474ea2c68a25d21d49d6b56088aad
Karim-Mane <karimanee@outlook.com>
2024-03-12 16:00:26 +0000

    add github actions from packagetemplate

--- tests/testthat/setup-options.R
+++ tests/testthat/setup-options.R
@@ -1,6 +1,6 @@
 # We want to flag partial matching as part of our testing & continuous
 # integration process because it makes code more brittle.
-options( # nolint: undesirable_function_linter
+options(
   warnPartialMatchAttr = TRUE,
   warnPartialMatchDollar = TRUE,
   warnPartialMatchArgs = TRUE
