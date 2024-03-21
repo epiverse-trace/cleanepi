f5ba676e43bb78dd26f1ce8b0cd670f221230636
bahadzie <bahadzie@gmail.com>
2024-03-21 16:24:12 +0000

    column names are now standardized by default

--- tests/testthat/test-column_name_standardisation.R
+++ tests/testthat/test-column_name_standardisation.R
@@ -1,16 +1,31 @@
 test_that("standardize_column_names works with rename argument", {
   cleaned_data <- standardize_column_names(
-    data   = readRDS(system.file("extdata", "test_df.RDS",
-                                 package = "cleanepi")),
+    data = readRDS(system.file("extdata", "test_df.RDS",
+      package = "cleanepi"
+    )),
     rename = c("dateOfBirth = DOB, sex=gender")
   )
   expect_s3_class(cleaned_data, "data.frame")
-  expect_named(cleaned_data, expected = c("study_id", "event_name",
-                                          "country_code", "country_name",
-                                          "date.of.admission", "DOB",
-                                          "date_first_pcr_positive_test",
-                                          "gender"))
+  expect_named(cleaned_data, expected = c(
+    "study_id", "event_name",
+    "country_code", "country_name",
+    "date.of.admission", "DOB",
+    "date_first_pcr_positive_test",
+    "gender"
+  ))
 })
+test_that("standardize_column_names fails when rename argument contains existing column names", {
+  expect_error(
+    standardize_column_names(
+      data = readRDS(system.file("extdata", "test_df.RDS",
+        package = "cleanepi"
+      )),
+      rename = c("dateOfBirth = DOB, sex=dateOfBirth")
+    ),
+    regexp = cat("Replace column names already exists")
+  )
+})
+
 
 test_that("standardize_column_names works with keep argument", {
   # TODO update this after Banky's suggestion as 'dateOfBirth' might change
@@ -19,65 +34,76 @@ test_that("standardize_column_names works with keep argument", {
     keep = "date.of.admission"
   )
   expect_s3_class(cleaned_data, "data.frame")
-  expect_named(cleaned_data, expected = c("study_id", "event_name",
-                                          "country_code", "country_name",
-                                          "date.of.admission", "dateOfBirth",
-                                          "date_first_pcr_positive_test",
-                                          "sex"))
+  expect_named(cleaned_data, expected = c(
+    "study_id", "event_name",
+    "country_code", "country_name",
+    "date.of.admission", "dateOfBirth",
+    "date_first_pcr_positive_test",
+    "sex"
+  ))
 })
 
 test_that("standardize_column_names works with all argument", {
   cleaned_data <- standardize_column_names(
-    data   = readRDS(system.file("extdata", "test_df.RDS",
-                                 package = "cleanepi")),
+    data = readRDS(system.file("extdata", "test_df.RDS",
+      package = "cleanepi"
+    )),
     rename = c("dateOfBirth = DOB, sex=gender"),
-    keep   = "date.of.admission"
+    keep = "date.of.admission"
   )
   expect_s3_class(cleaned_data, "data.frame")
-  expect_named(cleaned_data, expected = c("study_id", "event_name",
-                                          "country_code", "country_name",
-                                          "date.of.admission", "DOB",
-                                          "date_first_pcr_positive_test",
-                                          "gender"))
+  expect_named(cleaned_data, expected = c(
+    "study_id", "event_name",
+    "country_code", "country_name",
+    "date.of.admission", "DOB",
+    "date_first_pcr_positive_test",
+    "gender"
+  ))
 })
 
 test_that("standardize_column_names fails when 'linelist_tags' is provided when
           dealing with a data frame", {
-            expect_error(
-              standardize_column_names(
-                data   = readRDS(system.file("extdata", "test_df.RDS",
-                                             package = "cleanepi")),
-                rename = c("dateOfBirth = DOB, sex=gender"),
-                keep   = "linelist_tags"
-              ),
-              regexp = cat("Assertion on',keep,'failed: usage of 'linelist_tags'
+  expect_error(
+    standardize_column_names(
+      data = readRDS(system.file("extdata", "test_df.RDS",
+        package = "cleanepi"
+      )),
+      rename = c("dateOfBirth = DOB, sex=gender"),
+      keep = "linelist_tags"
+    ),
+    regexp = cat("Assertion on',keep,'failed: usage of 'linelist_tags'
                             is only reserved for 'linelist' type of data.")
-            )
-          })
+  )
+})
 
 test_that("standardize_column_names fails when wrong column names are
           specified", {
-            expect_error(
-              standardize_column_names(
-                data   = readRDS(system.file("extdata", "test_df.RDS",
-                                             package = "cleanepi")),
-                rename = c("date of birth = DOB, sex=gender"),
-                keep   = NULL
-              ),
-              regexp = cat("Assertion on',keep or rename,'failed: Only the
+  expect_error(
+    standardize_column_names(
+      data = readRDS(system.file("extdata", "test_df.RDS",
+        package = "cleanepi"
+      )),
+      rename = c("date of birth = DOB, sex=gender"),
+      keep = NULL
+    ),
+    regexp = cat("Assertion on',keep or rename,'failed: Only the
                            column names from the input data can be renamed or
                            kept.")
-            )
-          })
+  )
+})
 
 test_that("get_new_names works as expected", {
-  rename_idx <- get_new_names(original_names = c("study_id", "event_name",
-                                                 "country_code", "country_name",
-                                                 "date.of.admission",
-                                                 "dateOfBirth",
-                                                 "date_first_pcr_positive_test",
-                                                 "sex"),
-                              target_columns = "dateOfBirth = DOB, sex=gender")
+  rename_idx <- get_new_names(
+    original_names = c(
+      "study_id", "event_name",
+      "country_code", "country_name",
+      "date.of.admission",
+      "dateOfBirth",
+      "date_first_pcr_positive_test",
+      "sex"
+    ),
+    target_columns = "dateOfBirth = DOB, sex=gender"
+  )
   expect_type(rename_idx, "integer")
   expect_named(rename_idx, expected = c("DOB", "gender"))
   expect_length(rename_idx, n = 2L)
@@ -86,9 +112,11 @@ test_that("get_new_names works as expected", {
 test_that("get_new_names fails as expected", {
   expect_error(
     get_new_names(
-      original_names = c("study_id", "event_name", "country_code",
-                         "country_name", "date.of.admission", "dateOfBirth",
-                         "date_first_pcr_positive_test", "sex"),
+      original_names = c(
+        "study_id", "event_name", "country_code",
+        "country_name", "date.of.admission", "dateOfBirth",
+        "date_first_pcr_positive_test", "sex"
+      ),
       target_columns = "date of birth = DOB, sex=gender"
     ),
     regexp = cat("Assertion on',rename,'failed: Only the column names from the
