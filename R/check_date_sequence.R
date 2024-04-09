47821a3e936928627e4b7fc088aab674b22769e4
Karim-Mane <karimanee@outlook.com>
2024-04-09 02:42:31 +0000

    remove the internal call of standardize_dates() from check_date_sequence()

--- R/check_date_sequence.R
+++ R/check_date_sequence.R
@@ -11,6 +11,8 @@
 #'    When the input data is a `linelist` object, this parameter can be set to
 #'    `linelist_tags` if you wish to the date sequence across tagged columns
 #'    only.
+#'    The values in this column should be in the ISO format (2024-12-31).
+#'    Otherwise, use the `standardize_dates()` function to standardize them.
 #'
 #' @returns The input dataset. When found, the incorrect date sequences will be
 #'    stored in the report where they can be accessed using
@@ -18,9 +20,20 @@
 #' @export
 #'
 #' @examples
+#' # import the data
+#' data <- readRDS(system.file("extdata", "test_df.RDS", package = "cleanepi"))
+#'
+#' # standardize the date values
+#' data <- data |>
+#'   standardize_dates(
+#'     target_columns  = c("date_first_pcr_positive_test", "date.of.admission"),
+#'     error_tolerance = 0.4,
+#'     format          = NULL,
+#'     timeframe       = NULL
+#'   )
+#'
 #' good_date_sequence <- check_date_sequence(
-#'   data           = readRDS(system.file("extdata", "test_df.RDS",
-#'                                        package = "cleanepi")),
+#'   data           = data,
 #'   target_columns = c("date_first_pcr_positive_test", "date.of.admission")
 #' )
 check_date_sequence <- function(data, target_columns) {
@@ -43,14 +56,6 @@ check_date_sequence <- function(data, target_columns) {
     target_columns <- target_columns[-idx]
     if (length(target_columns) < 2L) {
       stop("\nAt least 2 event dates are required!")
-    }
-  }
-
-  # check and convert to Date if required
-  for (cols in target_columns) {
-    if (!lubridate::is.Date(data[[cols]])) {
-      data <- standardize_dates(data, cols, timeframe = NULL,
-                                error_tolerance = 0.5)
     }
   }
 
--- tests/testthat/test-check_date_sequence.R
+++ tests/testthat/test-check_date_sequence.R
@@ -10,12 +10,25 @@ test_that("is_date_sequence_ordered works as expected", {
   expect_false(res)
 })
 
+# import the data
+data <- readRDS(system.file("extdata", "test_df.RDS",
+                            package = "cleanepi"))
+
+# standardize the date values
+data <- data |>
+  standardize_dates(
+    target_columns  = c("date_first_pcr_positive_test",
+                        "date.of.admission"),
+    error_tolerance = 0.4,
+    format          = NULL,
+    timeframe       = NULL
+  )
+
 test_that("check_date_sequence sends a warning when incorrect column nams are
           found", {
             expect_error(
               check_date_sequence(
-                data           = readRDS(system.file("extdata", "test_df.RDS",
-                                                     package = "cleanepi")),
+                data           = data,
                 target_columns = c("date_first_pcr_positive_test",
                                    "date.of.admission", "fake_name")
               ),
@@ -25,8 +38,7 @@ test_that("check_date_sequence sends a warning when incorrect column nams are
 
             expect_warning(
               check_date_sequence(
-                data           = readRDS(system.file("extdata", "test_df.RDS",
-                                                     package = "cleanepi")),
+                data           = data,
                 target_columns = c("date_first_pcr_positive_test",
                                    "date.of.admission")              ),
               regexp = cat("Detected 2 incorrect date sequences at
@@ -35,8 +47,7 @@ test_that("check_date_sequence sends a warning when incorrect column nams are
 
             expect_error(
               check_date_sequence(
-                data           = readRDS(system.file("extdata", "test_df.RDS",
-                                                     package = "cleanepi")),
+                data           = data,
                 target_columns = c("date_first_pcr_positive_test",
                                    "fake_name")),
               regexp = cat("At least 2 event dates are required!")
@@ -45,19 +56,20 @@ test_that("check_date_sequence sends a warning when incorrect column nams are
 
 test_that("check_date_sequence works as expected when target_column is provided
           as a vector", {
-  good_date_sequence <- check_date_sequence(
-    data           = readRDS(system.file("extdata", "test_df.RDS",
-                                         package = "cleanepi")),
-    target_columns = c("date_first_pcr_positive_test", "date.of.admission")
-  )
-  report <- attr(good_date_sequence, "report")
-  expect_s3_class(good_date_sequence, "data.frame")
-  expect_identical(nrow(good_date_sequence), 10L)
-  expect_type(report, "list")
-  expect_named(report, "incorrect_date_sequence")
-  expect_s3_class(report[["incorrect_date_sequence"]], "data.frame")
-  expect_identical(nrow(report[["incorrect_date_sequence"]]), 2L)
-  expect_identical(ncol(report[["incorrect_date_sequence"]]), 2L)
-  expect_identical(names(report[["incorrect_date_sequence"]]),
-                   c("date_first_pcr_positive_test", "date.of.admission"))
+              good_date_sequence <- check_date_sequence(
+                data           = data,
+                target_columns = c("date_first_pcr_positive_test",
+                                   "date.of.admission")
+              )
+              report <- attr(good_date_sequence, "report")
+              expect_s3_class(good_date_sequence, "data.frame")
+              expect_identical(nrow(good_date_sequence), 10L)
+              expect_type(report, "list")
+              expect_named(report, "incorrect_date_sequence")
+              expect_s3_class(report[["incorrect_date_sequence"]], "data.frame")
+              expect_identical(nrow(report[["incorrect_date_sequence"]]), 2L)
+              expect_identical(ncol(report[["incorrect_date_sequence"]]), 2L)
+              expect_identical(names(report[["incorrect_date_sequence"]]),
+                               c("date_first_pcr_positive_test",
+                                 "date.of.admission"))
 })
