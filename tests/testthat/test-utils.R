f49ea2d7cdd9c5ab0ce0516ee6bf6fd0a01be7ab
Karim-Mane <karimanee@outlook.com>
2024-07-29 17:04:50 +0000

    refactor the scan_data() function

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -110,9 +110,9 @@ test_that("add_to_report works as expected", {
   expect_type(report, "list")
   expect_length(report, 1L)
   expect_named(report, "scanning_result")
-  expect_named(report[["scanning_result"]], c("Field_names", "missing",
-                                              "numeric", "date", "character",
-                                              "logical"))
+  expect_named(report[["scanning_result"]],
+               c("Field_names", "missing", "numeric", "date", "character",
+                 "logical", "date-time", "factor"))
   expect_identical(nrow(report[["scanning_result"]]), ncol(data))
 })
 
