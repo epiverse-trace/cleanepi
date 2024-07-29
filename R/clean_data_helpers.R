037c37b7eaa3683ed0efcaedab8157a3c73ec5da
Karim-Mane <karimanee@outlook.com>
2024-07-29 17:04:50 +0000

    refactor the scan_data() function

--- R/clean_data_helpers.R
+++ R/clean_data_helpers.R
@@ -1,60 +1,4 @@
 
-#' Calculate the percentage of missing and other data type values from a vector
-#' of factor or logical values.
-#'
-#' @param x A vector of ones or a combination of various data types.
-#'
-#' @returns A vector of 5 elements representing the percentage of missing,
-#' numeric, date, character, and logical values found in the input vector.
-#'
-#' @keywords internal
-#'
-scan_lgl_and_fct_columns <- function(x) {
-  ## for logical and factor columns, we will use their `levels` to determine
-  ## the proportion of the different types
-  are_numeric <- are_date <- are_logical <- are_character <- 0L
-
-  # save the variable length
-  n_rows <- length(x)
-
-  # get the proportion of NA
-  are_na <- round((sum(is.na(x)) / n_rows), 6L)
-  x      <- x[!is.na(x)]
-
-  # get the proportion of logical values
-  if (is.logical(x)) {
-    are_logical <- round((sum(x) + sum(!x)) / n_rows, 6L)
-    return(c(are_na, are_numeric, are_date, are_character, are_logical))
-  }
-
-  # get the proportion of numeric values
-  l <- levels(x)
-  numeric_levels <- l[!is.na(suppressWarnings(as.numeric(l)))]
-  if (length(numeric_levels) > 0L) {
-    are_numeric <- round((sum(x %in% numeric_levels) / n_rows), 6L)
-  }
-
-  date_levels <- l[!is.na(suppressWarnings(
-    as.Date(
-      lubridate::parse_date_time(
-        l,
-        orders = c("ymd", "ydm", "dmy", "mdy", "myd", "dym", "Ymd", "Ydm",
-                   "dmY", "mdY", "mYd", "dYm")
-      )
-    )
-  ))]
-  if (length(date_levels) > 0L) {
-    are_date <- round((sum(x %in% date_levels) / n_rows), 6L)
-  }
-
-  are_character <- round((1.0 - (are_na + are_numeric +
-                                   are_date + are_logical)), 6L)
-
-  # return the output
-  return(c(are_na, are_numeric, are_date, are_character, are_logical))
-}
-
-
 #' Calculate the percentage of missing and other data type values in a vector
 #' containing different data types such as numeric, Date, character, and
 #' logical.
@@ -68,76 +12,12 @@ scan_lgl_and_fct_columns <- function(x) {
 #' @keywords internal
 #'
 scan_columns <- function(x, type) {
-  ## The processing path is determine with the logical `verdict`
-  type    <- unlist(strsplit(type, ",", fixed = TRUE))
-  verdict <- type %in% c("logical", "factor")
-
-  ## logical and factor are processed differently from the other.
-  ## Logical vectors are expected to contain `TRUE` or `FALSE` or `NA`. For
-  ## columns of this type, we will check the %NA, %TRUE and %FALSE. The others
-  ## will be set at 0.
-  ## For factor columns, the processing is based on their levels. Levels will be
-  ## checked for numeric, date, logical, NA, and character
-  if (any(verdict)) {
-    return(scan_lgl_and_fct_columns(x))
-  }
-
-  # save the variable length
-  n_rows <- length(x)
-
-  # get the proportion of NA
-  are_na <- round((sum(is.na(x)) / n_rows), 6L)
-  x      <- x[!is.na(x)]
-
-  # get the proportion of date values
-  are_date <- 0L
-  if (any(type %in% c("POSIXct", "POSIXt"))) {
-    tmp      <- as.Date(x)
-    are_date <- round((sum(!is.na(tmp)) / n_rows), 6L)
-    x        <- x[is.na(tmp)]
-  }
-  if (!is.null(lubridate::guess_formats(x, c("ymd", "ydm", "dmy", "mdy", "myd",
-                                             "dym", "Ymd", "Ydm", "dmY", "mdY",
-                                             "mYd", "dYm")))) {
-    tmp    <- suppressWarnings(
-      as.Date(
-        lubridate::parse_date_time(
-          x, orders = c("ymd", "ydm", "dmy", "mdy", "myd", "dym", "Ymd", "Ydm",
-                        "dmY", "mdY", "mYd", "dYm")
-        )
-      )
-    )
-    are_date <- round((sum(!is.na(tmp)) / n_rows), 6L)
-    x        <- x[is.na(tmp)]
-  }
-
-  # get the proportion of numeric values
-  are_numeric <- are_logical <- 0L
-  if (length(x) > 0L) {
-    tmp         <- suppressWarnings(as.numeric(x))
-    are_numeric <- round((sum(!is.na(tmp)) / n_rows), 6L)
-  }
-
-  # get the proportion of character values
-  are_character   <- 0L
-  if (!all(verdict)) {
-    are_character <- round((1.0 - (are_na + are_numeric +
-                                     are_date + are_logical)), 6L)
-  }
-
-  # return the output
-  return(c(are_na, are_numeric, are_date, are_character, are_logical))
-}
-
-#' Get class of a vector
-#'
-#' @param x The input vector
-#'
-#' @return A character with the class of the input vector
-#' @keywords internal
-#'
-get_class <- function(x) {
-  paste(class(x), collapse = ",")
+  res <- switch(type,
+                double = scan_in_double(x),
+                integer = scan_in_integer(x),
+                logical = scan_in_logical(x),
+                character = scan_in_character(x))
+  return(res)
 }
 
 #' Scan a data frame to determine the percentage of `missing`, `numeric`,
@@ -157,18 +37,179 @@ get_class <- function(x) {
 #'                              package = "cleanepi"))
 #' )
 scan_data <- function(data) {
-  # when scanning through the data, logical and factor columns will be treated
-  # differently from the others. It means only the percent of missing and Date
-  # values will be evaluated for these columns.
-  # The percent of numeric and character value will be set automatically to 0 as
-  # to prevent from the effects of the conversion to numeric and character.
-  types       <- vapply(data, get_class, character(1L))
+  types       <- vapply(data, typeof, character(1L))
   scan_result <- vapply(seq_len(ncol(data)), function(col_index) {
     scan_columns(data[[col_index]], types[[col_index]])
-  }, numeric(5L))
+  }, numeric(7L))
   scan_result        <- as.data.frame(t(scan_result))
   names(scan_result) <- c("missing", "numeric", "date", "character",
-                             "logical")
+                          "logical", "date-time", "factor")
   scan_result        <- cbind(Field_names = names(data), scan_result)
   return(scan_result)
+}
+
+#' Scan through a double column
+#'
+#' @param x The input vector
+#'
+#' @return A numeric vector with the proportion of the different types of data
+#'    that were detected within the input vector.
+#' @keywords internal
+#'
+scan_in_double <- function(x) {
+  are_factor <- are_date <- are_date_time <- are_character <- are_numeric <-
+    are_logical <- are_na <- 0.0
+  # save the variable length
+  n_rows <- length(x)
+
+  # get the proportion of NA
+  are_na <- round((sum(is.na(x)) / n_rows), 6L)
+  x      <- x[!is.na(x)]
+
+  # doubles are either numeric (attributes = NULL), or Date (has a 'class'
+  # attributes = Date), or date-time (has a 'class' attributes = POSIXt)
+  if ("class" %in% names(attributes(x))) {
+    if ("Date" %in% attributes(x)[["class"]]) {
+      are_date <- round((length(x) / n_rows), 6L)
+    } else if ("POSIXt" %in% attributes(x)[["class"]]) {
+      are_date_time <- round((length(x) / n_rows), 6L)
+    }
+  } else {
+    are_numeric <- round((length(x) / n_rows), 6L)
+  }
+  return(
+    c(are_na, are_numeric, are_date, are_character, are_logical, are_date_time,
+      are_factor)
+  )
+}
+
+#' Scan through an integer column
+#'
+#' @param x The input vector
+#'
+#' @return A numeric vector with the proportion of the different types of data
+#'    that were detected within the input vector.
+#' @keywords internal
+#'
+scan_in_integer <- function(x) {
+  are_factor <- are_date <- are_date_time <- are_character <- are_numeric <-
+    are_logical <- are_na <- 0.0
+  # save the variable length
+  n_rows <- length(x)
+
+  # get the proportion of NA
+  are_na <- round((sum(is.na(x)) / n_rows), 6L)
+  x      <- x[!is.na(x)]
+
+  # integers are either numeric (attributes = NULL), or factors (has a 'class'
+  # and 'levels' attributes)
+  if (is.null(attributes(x))) {
+    are_numeric <- round((length(x) / n_rows), 6L)
+  } else if (identical(names(attributes(x)), c("levels", "class"))) {
+    are_factor <- round((length(x) / n_rows), 6L)
+  }
+
+  return(
+    c(are_na, are_numeric, are_date, are_character, are_logical, are_date_time,
+      are_factor)
+  )
+}
+
+#' Scan through a logical column
+#'
+#' @param x The input vector
+#'
+#' @return A numeric vector with the proportion of the different types of data
+#'    that were detected within the input vector.
+#' @keywords internal
+#'
+scan_in_logical <- function(x) {
+  are_factor <- are_date <- are_date_time <- are_character <- are_numeric <-
+    are_logical <- are_na <- 0.0
+
+  # logical are simply logical. We will only determine the %NA and %logical
+  # save the variable length
+  n_rows <- length(x)
+
+  # get the proportion of NA
+  are_na <- round((sum(is.na(x)) / n_rows), 6L)
+  x      <- x[!is.na(x)]
+
+  # get the proportion of logical
+  are_logical <- round((length(x) / n_rows), 6L)
+  return(
+    c(are_na, are_numeric, are_date, are_character, are_logical, are_date_time,
+      are_factor)
+  )
+}
+
+#' Scan through a character column
+#'
+#' @param x The input vector
+#'
+#' @return A numeric vector with the proportion of the different types of data
+#'    that were detected within the input vector.
+#' @keywords internal
+#'
+scan_in_character <- function(x) {
+  # There might be, within a character column, values of type:
+  # character, numeric, date, date-time, NA, and logical
+  # In this function, we check the presence of these different types within a
+  # character column.
+  # Note that numeric values can actually be of 'Date' or 'date-time' type.
+  # Given that any numeric can be converted into Date, we will not check for
+  # Date or date-time values within the numeric.
+
+  are_factor <- are_date <- are_date_time <- are_character <- are_numeric <-
+    are_logical <- are_na <- 0.0
+
+  # save the variable length
+  n_rows <- length(x)
+
+  # get the proportion of NA
+  are_na <- round((sum(is.na(x)) / n_rows), 6L)
+  x      <- x[!is.na(x)]
+
+  # get double values and evaluate the proportion numeric values
+  doubles <- x[!is.na(suppressWarnings(as.double(x)))]
+  if (length(doubles) > 0L) {
+    are_numeric <- round((length(doubles) / n_rows), 6L)
+  }
+
+  # get character values and check for the presence of Date and date-time
+  characters <- x[is.na(suppressWarnings(as.double(x)))]
+  if (length(characters) > 0L) {
+    # get the proportion of date values
+    if (!is.null(lubridate::guess_formats(characters,
+                                          c("ymd", "ydm", "dmy", "mdy", "myd",
+                                            "dym", "Ymd", "Ydm", "dmY", "mdY",
+                                            "mYd", "dYm")))) {
+      tmp    <- suppressWarnings(
+        as.Date(
+          lubridate::parse_date_time(
+            characters,
+            orders = c("ymd", "ydm", "dmy", "mdy", "myd", "dym", "Ymd", "Ydm",
+                       "dmY", "mdY", "mYd", "dYm")
+          )
+        )
+      )
+      are_date   <- round((sum(!is.na(tmp)) / n_rows), 6L)
+      x          <- x[is.na(tmp)]
+      characters <- characters[is.na(tmp)]
+    }
+  }
+
+  # get the proportion of logical values
+  logicals <- toupper(characters) == "TRUE" | toupper(characters) == "FALSE"
+  are_logical <- round((sum(logicals) / n_rows), 6L)
+
+  # get the proportion of character values
+  are_character <- round((1.0 - (are_na + are_numeric +
+                                   are_date + are_logical)), 6L)
+
+  # return the output
+  return(
+    c(are_na, are_numeric, are_date, are_character, are_logical, are_date_time,
+      are_factor)
+  )
 }
