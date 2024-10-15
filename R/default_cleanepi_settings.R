b810d64828aaa06d12a9169b9e1433e2b43fe9a9
Karim-Mane <karimanee@outlook.com>
2024-10-15 16:23:37 +0000

    update function documentations

--- R/default_cleanepi_settings.R
+++ R/default_cleanepi_settings.R
@@ -1,7 +1,7 @@
-#' Set `clean_data()` default parameters
+#' Set and return \code{clean_data()} default parameters
 #'
-#' When calling `clean_data()` function with `param = NULL`, the arguments
-#' defined here will applied on the input data. By default, operations that
+#' When calling `clean_data()` function with `params = NULL`, the arguments
+#' defined here will be applied on the input data. By default, operations that
 #' require the target columns to specified by the user will not be performed.
 #' The default cleaning operations include: i) standardizing column names,
 #' ii) detecting and removing duplicates, and iii) removing constant data.
@@ -9,20 +9,39 @@
 #' @return The list of the default cleaning parameters.
 #' @keywords internal
 #'
-default_cleanepi_settings <- function() {
+get_default_params <- function() {
   params <- list(
     standardize_column_names = list(keep = NULL, rename = NULL),
     replace_missing_values = NULL,
-    remove_duplicates = list(
-      target_columns   = NULL
-    ),
+    remove_duplicates = list(target_columns = NULL),
     remove_constants = list(cutoff = 1L),
     standardize_dates = NULL,
     standardize_subject_ids = NULL,
     dictionary = NULL,
     to_numeric = NULL,
-    check_date_sequence = NULL,
-    span = NULL
+    check_date_sequence = NULL
   )
   return(params)
+}
+
+#' Update \code{clean_data()} default argument's values with the user-provided
+#' values
+#'
+#' @param defaults A list with the default arguments
+#' @param params A list with the user-specified arguments
+#' @param strict A boolean that specified whether to trigger an error or not
+#'    when there is a difference between the list of default arguments and
+#'    list of the arguments provided by the user.
+#'
+#' @return The updated list of parameters that will be used to perform the data
+#'    cleaning.
+#' @keywords internal
+#'
+modify_default_params <- function(defaults, params, strict = TRUE) {
+  extra <- setdiff(names(params), names(defaults))
+  if (strict && (length(extra) > 0L)) {
+    stop("Additional invalid options: ", toString(extra))
+  }
+  # keep.null is needed here
+  return(utils::modifyList(defaults, params, keep.null = TRUE))
 }
