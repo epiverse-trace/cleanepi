c9a9d28cebcab555c3776131549adc47925f999d
Karim-Mane <karimanee@outlook.com>
2024-11-07 12:14:25 +0000

    configure package for translation

---
+++ R/utils-potools.R
@@ -0,0 +1,13 @@
+#' Flag out what message will be translated using {potools}
+#'
+#' @param ... A character string. This represents the message to be translated
+#'
+#' @return The input object
+#' @keywords internal
+#' @details
+#' This function was taken from the `Translation for package developers`
+#' vignette of the \code{{potools}} package.
+#'
+tr_ <- function(...) {
+  enc2utf8(gettext(paste0(...), domain = "R-pockage"))
+}
