ce354f5f001d0596aee10e09904e9d5b6c13d30b
Karim-Mane <karimanee@outlook.com>
2024-11-12 11:58:40 +0000

    update messages in default_cleaning_settings.R file

--- R/default_cleanepi_settings.R
+++ R/default_cleanepi_settings.R
@@ -45,9 +45,9 @@ modify_default_params <- function(defaults, params, strict = TRUE) {
   extra <- setdiff(names(params), names(defaults))
   if (strict && (length(extra) > 0L)) {
     cli::cli_abort(c(
-      tr_("Found the following unrecognised arguments to `clean_data()`: {.code {toString(extra)}}."), # nolint: line_length_linter
-      i = tr_("`clean_data()` does not support arguments other than the defaults. Run get_default_params() to display the list of default parameters."), # nolint: line_length_linter
-      x = tr_("You provided more arguments than expected.")
+      tr_("Found the following unrecognised arguments to {.fn clean_data}: {.emph {toString(extra)}}."), # nolint: line_length_linter
+      i = tr_("{.fn clean_data} does not support arguments other than the defaults. Run {.code get_default_params()} to display the list of default parameters."), # nolint: line_length_linter
+      x = tr_("You provided unexpected arguments.")
     ))
   }
   # keep.null is needed here
