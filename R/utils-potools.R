1649e025bdc8ccfe521ebd53f0f831ee49af4c3a
Karim-Mane <karimanee@outlook.com>
2024-12-16 12:10:47 +0000

    update function documentations

--- R/utils-potools.R
+++ R/utils-potools.R
@@ -1,12 +1,12 @@
-#' Flag out what message will be translated using \{potools\}
+#' Flag out what message will be translated using the \pkg{potools} package
 #'
 #' @param ... A character string. This represents the message to be translated
 #'
 #' @return The input object
 #' @keywords internal
 #' @details
-#' This function was taken from the `Translation for package developers`
-#' vignette of the \code{\{potools\}} package.
+#' This function was copied from the `Translation for package developers`
+#' vignette of the \pkg{potools} package.
 #'
 tr_ <- function(...) {
   enc2utf8(gettext(paste0(...), domain = "R-cleanepi"))
