6fd879248d337cd62a839a88f18070f74495cef3
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-03-21 16:11:00 +0000

    add utils::menu to clean_spelling_mistakes

--- R/clean_spelling_mistakes.R
+++ R/clean_spelling_mistakes.R
@@ -1,10 +1,24 @@
 #' Clean spelling mistakes detected with approximate string matching and
 #' replaced with `wordlist`
 #'
+#' @details
+#' When used interactively (see [interactive()]) the user is presented a menu
+#' to ensure that the words detected using approximate string matching are not
+#' false positives and the user can decided whether to proceed with the
+#' spelling corrections. In non-interactive sessions all spelling mistakes
+#' detected with approximate string matching are replaced by the `wordlist`
+#' word.
+#'
 #' @inheritParams clean_data
 #' @param wordlist A `character` vector or list of words to match to cells in
 #' `data`.
 #' @inheritParams base::agrep
+#' @param confirm A `logical` boolean to determine whether to show the user a
+#' menu of spelling corrections. If `TRUE` and using \R interactively then the
+#' user will have the option to review the proposed spelling changes. (This
+#' argument is useful for turning off the [menu()] when
+#' [rlang::is_interactive()] returns `TRUE` but not wanting to prompt the user
+#' e.g. `devtools::run_examples()`).
 #'
 #' @return The cleaned input date according to the user-specified `wordlist`.
 #' @export
@@ -17,27 +31,54 @@
 #' df
 #' clean_spelling_mistakes(
 #'   data = df,
-#'   wordlist = c("confirmed", "probable", "suspected", "died", "recovered")
+#'   wordlist = c("confirmed", "probable", "suspected", "died", "recovered"),
+#'   confirm = FALSE
 #' )
 clean_spelling_mistakes <- function(data,
                                     wordlist,
                                     max.distance = 1,
-                                    ignore.case = FALSE) {
+                                    ignore.case = FALSE,
+                                    confirm = TRUE) {
   checkmate::assert_data_frame(data, null.ok = FALSE, min.cols = 1L)
-  # convert wordlist to vector is supplied as list
+  checkmate::assert_logical(confirm, any.missing = FALSE, len = 1)
+  # convert wordlist to vector in case supplied as a list
   wordlist <- unlist(wordlist)
 
   for (col in seq_len(ncol(data))) {
     for (word in seq_along(wordlist)) {
-      idx <- agrepl(
-        pattern = wordlist[word],
-        x = data[, col],
-        max.distance = max.distance,
-        ignore.case = ignore.case
-      )
-      data[, col][idx] <- wordlist[word]
+      # only check and fix char columns
+      if (is.character(data[, col])) {
+        idx <- agrepl(
+          pattern = wordlist[word],
+          x = data[, col],
+          max.distance = max.distance,
+          ignore.case = ignore.case
+        )
+        # remove correct spelling from matches
+        idx[data[, col] == wordlist[word]] <- FALSE
+        if (any(idx)) {
+          # only show user menu when interactive
+          if (rlang::is_interactive() && confirm) {
+            # only print each misspelled word once
+            misspelled <- unique(data[, col][idx])
+            browser()
+            menu_title <- paste(
+              "The following words will be corrected:",
+              toString(paste("\n -", misspelled, "->", wordlist[word])),
+              "\n\n (0 to exit)"
+            )
+            # ask user to change spelling if fuzzy matched
+            user_choice <- utils::menu(
+              choices = c("Yes", "No"),
+              title = menu_title
+            )
+            if (user_choice == 1) data[, col][idx] <- wordlist[word]
+          } else {
+            data[, col][idx] <- wordlist[word]
+          }
+        }
+      }
     }
   }
-
   return(data)
 }
