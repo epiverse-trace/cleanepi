d4085f1136e8ecb5a757a6f819c8666c99cbdd33
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-03-21 16:47:06 +0000

    add unit test for clean_spelling_mistakes

---
+++ tests/testthat/test-clean_spelling_mistakes.R
@@ -0,0 +1,19 @@
+test_that("clean_spelling_mistakes works as expected by default", {
+  df <- data.frame(
+    case_type = c("confirmed", "confermed", "probable", "susspected"),
+    outcome = c("died", "recoverd", "did", "recovered")
+  )
+  clean_df <- clean_spelling_mistakes(
+    data = df,
+    wordlist = c("confirmed", "probable", "suspected", "died", "recovered"),
+    confirm = FALSE
+  )
+  expect_identical(
+    clean_df$case_type,
+    c("confirmed", "confirmed", "probable", "suspected")
+  )
+  expect_identical(
+    clean_df$outcome,
+    c("died", "recovered", "died", "recovered")
+  )
+})
