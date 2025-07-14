52bc95e70d3aac842d99c0fcd3822da6146e24de
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-07-14 17:28:58 +0100

    fix linters

--- R/date_standardization_helpers.R
+++ R/date_standardization_helpers.R
@@ -396,7 +396,7 @@ date_detect_day_or_month <- function(x) {
   all_abb_months <- c(month.abb, toupper(month.abb), tolower(month.abb))
   all_full_days <- c(full_days, toupper(full_days), tolower(full_days))
   all_abb_days <- c(abreviated_days, toupper(abreviated_days),
-                       tolower(abreviated_days))
+                    tolower(abreviated_days))
   in_full_month <- x %in% all_full_months
   in_abb_month <- x %in% all_abb_months
   in_full_day <- x %in% all_full_days
